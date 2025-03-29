1df5b638e72307b24c6d04b6c965b1c21f2a7cee
unknown <saifurrahimwp@gmail.com>
2025-03-29 19:53:39 +0700

    Initialize OCR Module : for AI based OCR

---
+++ AiShowcaseWeb/Controllers/OCRController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AiShowcaseWeb.Controllers
+{
+    public class OCRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
